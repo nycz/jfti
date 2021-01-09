1760766ca26ae65f00ab9e4ae40558c17f1d0578
nycz <pyntix@gmail.com>
2021-01-09 21:01:23 +0100

    Set version to 0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jfti',
-    version='0.1',
+    version='0.5.0',
     description='library for reading and editing XMP tags',
     url='https://github.com/nycz/jfti',
     author='nycz',
